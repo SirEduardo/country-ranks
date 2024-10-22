d05012551293b7e6b9dc8542f8753991a9c38fda
SirEduardo <eduardosaanchezlopez@gmail.com>
2024-10-22 13:48:11 +0200

    Update types.d.ts

--- src/types.d.ts
+++ src/types.d.ts
@@ -1,10 +1,12 @@
 export interface Country {
     id:string
     name: CountryName
+    cca3: string
     population: number
     area: number
     region: string
     flag: string
+    flags: string
     capital: string
     subregion: string
     language: string[]
