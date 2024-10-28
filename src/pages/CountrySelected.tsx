74a1779c65280cf0e8011a5517c4ec66335bb11d
SirEduardo <eduardosaanchezlopez@gmail.com>
2024-10-28 14:28:21 +0100

    Update CountrySelected.tsx

--- src/pages/CountrySelected.tsx
+++ src/pages/CountrySelected.tsx
@@ -1,14 +1,17 @@
-import { useParams } from "react-router-dom"
+import { Link, useParams } from "react-router-dom"
 import Hero from "../components/Hero"
-import { useEffect, useState } from "react"
+import React, { useEffect, useState } from "react"
 import { Country } from "../types"
 
-const CountrySelected = () => {
+const CountrySelected: React.FC = () => {
   const { countryName } = useParams<{ countryName: string }>()
   const [country, setCountry] = useState<Country | null>(null)
   const [languages, setLanguages] = useState<[string, string][]>([])
   const [currencies, setCurrencies] = useState<
     [string, { name: string; symbol: string }][]
+  >([])
+  const [bordersInfo, setBordersInfo] = useState<
+    { name: string; flag: string; index: string }[]
   >([])
 
   const getCountry = async () => {
@@ -17,18 +20,37 @@ const CountrySelected = () => {
         `https://restcountries.com/v3.1/alpha/${countryName}`
       )
       const country = await response.json()
+
       if (country.length > 0) {
-        const languages = Object.entries(country[0].languages) as [
+        const countryData = country[0]
+
+        const languages = Object.entries(countryData.languages) as [
           string,
           string
         ][]
-        const currencies = Object.entries(country[0].currencies) as [
+        setLanguages(languages)
+        const currencies = Object.entries(countryData.currencies) as [
           string,
           { name: string; symbol: string }
         ][]
         setCurrencies(currencies)
-        setLanguages(languages)
-        setCountry(country[0])
+        const borders = countryData.borders || []
+        setCountry(countryData)
+
+        const borderDetails = await Promise.all(
+          borders.map(async (borderCode: string) => {
+            const borderResponse = await fetch(
+              `https://restcountries.com/v3.1/alpha/${borderCode}`
+            )
+            const borderCountry = await borderResponse.json()
+            return {
+              name: borderCountry[0].name.common,
+              flag: borderCountry[0].flags.png,
+              index: borderCountry[0].cca3,
+            }
+          })
+        )
+        setBordersInfo(borderDetails)
       }
     } catch (error) {
       console.error("Error fetching country:", error)
@@ -47,7 +69,7 @@ const CountrySelected = () => {
         {country ? (
           <div
             className="bg-Back-color flex flex-col justify-center"
-            key={country.name.common}
+            key={country.cca3}
           >
             <div className="w-full">
               <div className="flex flex-col justify-center items-center gap-5 -translate-y-6">
@@ -79,7 +101,7 @@ const CountrySelected = () => {
               </div>
             </div>
             <section className="mt-10">
-              <table className="w-full flex justify-between px-4">
+              <table className="w-full flex justify-between px-4 ">
                 <thead>
                   <tr className="flex flex-col gap-5 text-left text-Text-color opacity-70 text-sm">
                     <th>capital</th>
@@ -108,6 +130,24 @@ const CountrySelected = () => {
                   </tr>
                 </tbody>
               </table>
+              <p className="text-Text-color opacity-70 text-sm px-4 pt-5">
+                Neighbouring Countries
+              </p>
+              <div className="flex flex-wrap gap-3 p-3 justify-center">
+                {bordersInfo.map(({ name, flag, index }) => (
+                  <Link key={index} to={`/${country.cca3}`}>
+                    <div className="flex flex-col items-center">
+                      <img
+                        src={flag}
+                        alt={`Flag of ${name}`}
+                        className="h-16 w-20 rounded-md"
+                      />
+
+                      <p className="text-Text-color">{name}</p>
+                    </div>
+                  </Link>
+                ))}
+              </div>
             </section>
           </div>
         ) : (
