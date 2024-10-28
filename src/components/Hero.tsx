09c607da1a83d7795eb74dcc208b8bcc52c5140a
SirEduardo <eduardosaanchezlopez@gmail.com>
2024-10-28 11:30:11 +0100

    Update Hero.tsx

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -3,10 +3,13 @@ const Hero = () => {
     <div className="relative">
       <img
         className="h-32 w-full"
-        src="/src/assets/hero-image-wr.jpg"
+        src="/public/images/hero-image-wr.jpg"
         alt="hero-image"
       />
-      <img className="absolute top-2/4 right-1/4" src="/src/assets/Logo.svg" />
+      <img
+        className="absolute top-2/4 right-1/4"
+        src="/public/images/Logo.svg"
+      />
     </div>
   )
 }
