ab88ba45497e0d0a2fb5ed68b1ea5ddca492f4e9
SirEduardo <eduardosaanchezlopez@gmail.com>
2024-10-29 11:36:14 +0100

    Update Countries.tsx

--- src/pages/Countries.tsx
+++ src/pages/Countries.tsx
@@ -4,6 +4,7 @@ import Search from "../components/Search"
 import SortBy from "../components/SortBy"
 import { Link } from "react-router-dom"
 import Hero from "../components/Hero"
+import Status from "../components/Status"
 
 const Countries: React.FC = () => {
   const [countries, setCountries] = useState<Country[]>([])
@@ -55,61 +56,64 @@ const Countries: React.FC = () => {
         </div>
         <Search countries={countries} setCountries={setFilteredCountries} />
       </header>
-      <section className="flex flex-col gap-4 p-5 ">
-        <SortBy
-          countries={filteredCountries}
-          setCountries={setFilteredCountries}
-        />
-        <div>
-          <span className="text-Text-color opacity-70">Region</span>
-          <div className="flex pt-2 gap-3 justify-center flex-wrap">
-            {regions.map((region) => (
-              <button
-                key={region}
-                value={region}
-                onClick={() => handleSelect(region)}
-                className={`text-Text-color bg-Back-modal px-3 py-1 rounded-2xl ${
-                  selected.includes(region) ? "bg-blue-500" : ""
-                }`}
-              >
-                <span>{region}</span>
-              </button>
-            ))}
+      <div className="flex max-lg:flex-col">
+        <section className="flex flex-col gap-4 p-5 ">
+          <SortBy
+            countries={filteredCountries}
+            setCountries={setFilteredCountries}
+          />
+          <div>
+            <span className="text-Text-color opacity-70">Region</span>
+            <div className="flex pt-2 gap-3 justify-center flex-wrap">
+              {regions.map((region) => (
+                <button
+                  key={region}
+                  value={region}
+                  onClick={() => handleSelect(region)}
+                  className={`text-Text-color bg-Back-modal px-3 py-1 rounded-2xl ${
+                    selected.includes(region) ? "bg-blue-500" : ""
+                  }`}
+                >
+                  <span>{region}</span>
+                </button>
+              ))}
+            </div>
           </div>
-        </div>
-        <div>
-          <span className="text-Text-color opacity-70">Status</span>
-        </div>
-      </section>
-      <div className="px-5">
-        <table className="w-full">
-          <thead>
-            <tr className="text-left border-b border-gray-500 text-Text-color opacity-70 text-sm">
-              <th className="py-2 px4">Flag</th>
-              <th className="py-2 px4">Name</th>
-              <th className="py-2 px4">Population</th>
-              <th className="py-2 px4">Area (km²)</th>
-            </tr>
-          </thead>
-          <tbody>
-            {filteredCountries.map((country) => (
-              <tr key={country.name.common}>
-                <td className="py-2 px-2 text-xl lg:px-4 lg:text-5xl">
-                  {country.flag}
-                </td>
-                <td className="py-2 px-2 lg:px-4 text-Text-color">
-                  <Link to={`/${country.cca3}`}>{country.name.common}</Link>
-                </td>
-                <td className="py-2 px-2 lg:px-4 text-Text-color">
-                  {country.population.toLocaleString()}
-                </td>
-                <td className="py-2 px-2 lg:px-4 text-Text-color">
-                  {country.area.toLocaleString()}
-                </td>
+          <Status
+            countries={filteredCountries}
+            setCountries={setFilteredCountries}
+          />
+        </section>
+        <div className="px-5 w-full">
+          <table className="w-full">
+            <thead>
+              <tr className="text-left border-b border-gray-500 text-Text-color opacity-70 text-sm">
+                <th className="py-2 px4">Flag</th>
+                <th className="py-2 px4">Name</th>
+                <th className="py-2 px4">Population</th>
+                <th className="py-2 px4">Area (km²)</th>
               </tr>
-            ))}
-          </tbody>
-        </table>
+            </thead>
+            <tbody>
+              {filteredCountries.map((country) => (
+                <tr key={country.name.common}>
+                  <td className="py-2 px-2 text-xl lg:px-4 lg:text-5xl">
+                    {country.flag}
+                  </td>
+                  <td className="py-2 px-2 lg:px-4 lg:text-lg font-semibold text-Text-color">
+                    <Link to={`/${country.cca3}`}>{country.name.common}</Link>
+                  </td>
+                  <td className="py-2 px-2 lg:px-4 lg:text-lg font-semibold text-Text-color">
+                    {country.population.toLocaleString()}
+                  </td>
+                  <td className="py-2 px-2 lg:px-4 lg:text-lg font-semibold text-Text-color">
+                    {country.area.toLocaleString()}
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
       </div>
     </main>
   )
